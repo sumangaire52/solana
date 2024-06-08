0abba30e6d2aa9786ec554a93246e751571bc376
suman <suman.gaire@nimble-cr.com>
2024-06-08 18:19:36 +0545

    write script to generate keypair

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,11 @@
+import "dotenv/config"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers"
+
+const keypair = getKeypairFromEnvironment["SECRET_KEY"]
+console.log("Loaded keypair from environment")
+
+//import { Keypair } from "@solana/web3.js";
+// const keypair = Keypair.generate()
+// console.log("Key pair generated.")
+// console.log("Public key: ", keypair.publicKey.toBase58())
+// console.log("Secret key: ", keypair.secretKey)
